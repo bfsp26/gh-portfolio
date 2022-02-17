2fd77610e580c69a80727fe1979c1765160e383d
Bryan <bryan-_1997@outlook.com>
2022-02-17 14:59:07 -0500

    Enhancing SEO capabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -6,7 +7,23 @@ import { environment } from 'src/environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gh-portfolio';
+export class AppComponent implements OnInit {
   username = environment.username;
+
+  constructor(private title: Title, private meta: Meta) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('GitHub portfolio app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s GitHub portfolio`
+      },
+      {
+        name: 'author',
+        content: this.username
+      }
+    ]);
+  }
+
 }
